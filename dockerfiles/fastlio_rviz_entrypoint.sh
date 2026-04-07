283ca8ffd22b7ef96963d66f06799868650f3ea4
WarriorHanamy <632029496@qq.com>
2026-04-07 16:39:48 +0800

    remove problems.

--- dockerfiles/fastlio_rviz_entrypoint.sh
+++ dockerfiles/fastlio_rviz_entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
+# ROS2 setup.bash references unbound variables (e.g. AMENT_TRACE_SETUP_FILES).
+# Temporarily disable nounset around sourcing to avoid "unbound variable" errors.
+set +u
 source /opt/ros/humble/setup.bash
+set -u
 
 exec "$@"
--- dockerfiles/ros_entrypoint.sh
+++ dockerfiles/ros_entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
+# ROS2 setup.bash references unbound variables (e.g. AMENT_TRACE_SETUP_FILES).
+# Temporarily disable nounset around sourcing to avoid "unbound variable" errors.
+set +u
 source /opt/ros/humble/setup.bash
 
 WS_SETUP="${WS_DIR:-/root/ros2_ws}/install/setup.bash"
 if [ -f "$WS_SETUP" ]; then
     source "$WS_SETUP"
 fi
+set -u
 
 exec "$@"
